db02f61aee0d563840dcda38dfb203ed20ca1f13
lukaszbajor <48011715+lukaszbajor@users.noreply.github.com>
2021-03-09 23:28:27 +0100

    first linesof code

---
+++ main.js
@@ -0,0 +1,38 @@
+const animals = [
+  {
+    type: "Dog",
+    name: "Azor",
+    age: 2,
+    sex: "M",
+    describe: "Small dog in country",
+  },
+  {
+    type: "Cat",
+    name: "Mea",
+    age: 1,
+    sex: "F",
+    describe: "Small cat in country",
+  },
+  {
+    type: "Hamster",
+    name: "Fini",
+    age: 1.5,
+    sex: "M",
+    describe: "Small hamster in country",
+  },
+  {
+    type: "Dog",
+    name: "Ksenia",
+    age: 3.5,
+    sex: "F",
+    describe: "Small dog in country",
+  },
+  {
+    type: "Turtle",
+    name: "John",
+    age: 10,
+    sex: "M",
+    describe: "Big turtle in country",
+  },
+];
+console.log(animals);
