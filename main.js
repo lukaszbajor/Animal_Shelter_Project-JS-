07156e12b8cdb0248d3a6521d7a4a4dc67626813
lukaszbajor <48011715+lukaszbajor@users.noreply.github.com>
2021-05-24 16:22:43 +0200

    repair reset select options | repair selection of animals

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//Array Animals
 let animals = [
   {
     type: "Dog",
@@ -72,7 +73,9 @@ let animals = [
     id: 8,
   },
 ];
+//-----------------------------------------------------
 
+//Variables
 const box = document.querySelector(".items");
 const dogs = document.querySelector(".dogs");
 const cats = document.querySelector(".cats");
@@ -80,6 +83,7 @@ const horses = document.querySelector(".horses");
 const allAnimal = document.querySelector(".all");
 const countOfPlace = document.querySelector(".count");
 
+//Show animals with Array
 const showAllAnimal = () => {
   countOfPlace.textContent = animals.length;
   box.textContent = "";
@@ -107,57 +111,83 @@ const showAllAnimal = () => {
 };
 
 showAllAnimal();
+// const showElements = document.querySelector(".show-elements");
 
-// const showDogs = () => {
-//   showAllAnimal();
-//   const sd = [...document.getElementsByClassName("dog")];
-//   box.innerHTML = "";
-//   sd.forEach((item) => {
-//     box.innerHTML += `<div class="item">${item.innerHTML} </div>`;
-//   });
-// };
+const showDogs = () => {
+  showAllAnimal();
+  const all = [...document.getElementsByClassName("item")];
 
-// const showCats = () => {
-//   showAllAnimal();
-//   const sc = [...document.getElementsByClassName("cat")];
-//   box.innerHTML = "";
+  all.filter((item) => {
+    if (!item.classList.contains("dog")) {
+      item.classList.add("show-elements");
+    }
+  });
+};
 
-//   sc.forEach((item) => {
-//     deleteAnimal();
-//     box.innerHTML += `<div class="item">${item.innerHTML} </div>`;
-//   });
-// };
+const showCats = () => {
+  showAllAnimal();
+  const all = [...document.getElementsByClassName("item")];
 
-// const showHorses = () => {
-//   showAllAnimal();
-//   const sh = [...document.getElementsByClassName("horse")];
-//   box.innerHTML = "";
+  all.filter((item) => {
+    if (!item.classList.contains("cat")) {
+      item.classList.add("show-elements");
+    }
+  });
+};
 
-//   sh.forEach((item) => {
-//     box.innerHTML += `<div class="item">${item.innerHTML} </div>`;
-//   });
-// };
+const showHorses = () => {
+  showAllAnimal();
+  const all = [...document.getElementsByClassName("item")];
 
-// dogs.addEventListener("click", showDogs);
-// cats.addEventListener("click", showCats);
-// horses.addEventListener("click", showHorses);
-// allAnimal.addEventListener("click", () => {
-//   // box.innerHTML = "";
-//   showAllAnimal();
-// });
+  all.filter((item) => {
+    if (!item.classList.contains("horse")) {
+      item.classList.add("show-elements");
+    }
+  });
+};
 
+dogs.addEventListener("click", showDogs);
+cats.addEventListener("click", showCats);
+horses.addEventListener("click", showHorses);
+
+const showAll = () => {
+  showAllAnimal();
+  const all = [...document.getElementsByClassName("item")];
+
+  all.filter((item) => {
+    if (
+      item.classList.contains("horse") ||
+      item.classList.contains("dog") ||
+      item.classList.contains("cat")
+    ) {
+      item.classList.remove("show-elements");
+    }
+  });
+};
+
+allAnimal.addEventListener("click", showAll);
+
+//--------------------------------------------------
 //ADD PANEL
-
+//--------------------------------------------------
 const addPanel = document.querySelector(".add-panel");
 const btnAdd = document.querySelector(".btn-add");
+const closePanel = document.querySelector(".add-panel-X");
+//Show/Close Panel
 const showAddPanel = () => {
   addPanel.classList.add("reset");
 };
-
 btnAdd.addEventListener("click", showAddPanel);
 
-//CHANGE COUNT
+const closeAddPanel = () => {
+  addPanel.classList.remove("reset");
+};
 
+closePanel.addEventListener("click", closeAddPanel);
+
+//----------------------------------------------------
+//CHANGE COUNT
+//----------------------------------------------------
 const countOfAnimal = () => {
   countOfPlace.textContent = animals.length;
 };
@@ -181,42 +211,57 @@ const deleteAnimal = (id) => {
 const inpName = document.querySelector(".inpname");
 const inpAge = document.querySelector(".inpage");
 const inpSex = document.querySelector(".inpsex");
-const selectValue = document.getElementById("type");
+let selectValue = document.getElementById("type");
 const taDescribe = document.querySelector(".inpdescribe");
 const addAnimalBtn = document.querySelector(".add-animal");
 
 const addAnimal = () => {
   const name = inpName.value;
   const sex = inpSex.value;
+  let select = selectValue.options[selectValue.selectedIndex].text;
   const age = inpAge.value;
   const describe = taDescribe.value;
   let id = animals.length;
-
-  const div = document.createElement("div");
-  div.classList.add("item");
-  div.dataset.animalId = id;
-  div.innerHTML = `
+  if (animals.length < 30) {
+    const div = document.createElement("div");
+    div.classList.add("item");
+    div.dataset.animalId = id;
+    div.innerHTML = `
           <h2 class="name">${name}</h2>
           <p class="sex">Sex: ${sex}</p>
           <p class="age">Age: ${age}</p>
           <p class="describe">"${describe}"</p>
-          <p class="type">DOG</p>
+          <p class="type">${select}</p>
           <div class="buttons">
             <button class="edit">Edit</button>
             <button class="delete" onclick=deleteAnimal(${id})>Delete</button>
           </div>
           `;
-  animals.push({
-    type: "XD",
-    name: name,
-    age: age,
-    sex: sex,
-    describe: describe,
-    id: id,
-  });
+    animals.push({
+      type: "XD",
+      name: name,
+      age: age,
+      sex: sex,
+      describe: describe,
+      id: id,
+    });
 
-  countOfAnimal();
+    countOfAnimal();
 
-  box.appendChild(div);
+    box.appendChild(div);
+    resetData();
+  }
 };
 addAnimalBtn.addEventListener("click", addAnimal);
+//--------------------------------------------------
+
+//Reset data with panel
+const resetBtn = document.querySelector(".reset-data");
+const resetData = () => {
+  inpName.value = "";
+  inpAge.value = "";
+  inpSex.value = "";
+  taDescribe.value = "";
+  select = selectValue.options[(selectValue.selectedIndex = 0)].text;
+};
+resetBtn.addEventListener("click", resetData);
